ace5163f4e7783241c1392b8a5029e6e9c63d937
Aleksandra Rykova <rykaleand@gmail.com>
2026-03-10 19:52:09 +0300

    Practice 2 tasks

--- NauJava3/src/main/java/ru/nau/task3/Task3.java
+++ NauJava3/src/main/java/ru/nau/task3/Task3.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Task3 {
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("Иванов Иван Иванович", 25, "IT", 80000.0));
-        employees.add(new Employee("Петров Пётр Петрович", 35, "HR", 120000.0));
-        employees.add(new Employee("Сидорова Анна Сергеевна", 28, "IT", 95000.0));
-        employees.add(new Employee("Козлов Дмитрий Олегович", 42, "Finance", 150000.0));
-        employees.add(new Employee("Новикова Мария Игоревна", 31, "HR", 110000.0));
+        employees.add(new Employee("Ivanov Ivan Ivanovich", 25, "IT", 80000.0));
+        employees.add(new Employee("Petrov Petr Petrovich", 35, "HR", 120000.0));
+        employees.add(new Employee("Sidorova Anna Sergeevna", 28, "IT", 95000.0));
+        employees.add(new Employee("Kozlov Dmitry Olegovich", 42, "Finance", 150000.0));
+        employees.add(new Employee("Novikova Maria Igorevna", 31, "HR", 110000.0));
 
-        System.out.println("Есть ли сотрудник с зарплатой более 100000.00: " + employees.stream()
+        System.out.println("Is there an employee with salary more than 100000.00: " + employees.stream()
                 .anyMatch(e -> e.getSalary() > 100000.0));
     }
 }
